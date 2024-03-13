4486ac5f6f8fd521d76a9791ec43f8170f70dc70
solidados <geekbrains.pk@internet.ru>
2024-03-13 17:59:05 +0400

    feat: add route for req PUT single book by :id in index.js

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,5 @@
 const PORT = 5000;
-const DB_URL='mongodb+srv://guest:guest@tumobooksstore.gagrvt7.mongodb.net/tumobooks-store?retryWrites=true&w=majority&appName=TuMoBooksStore'
+const DB_URL =
+  'mongodb+srv://guest:guest@tumobooksstore.gagrvt7.mongodb.net/tumobooks-store?retryWrites=true&w=majority&appName=TuMoBooksStore';
 
-export {
-  PORT,
-  DB_URL
-}
+export { PORT, DB_URL };
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { PORT, DB_URL } from "./config.js";
+import { PORT, DB_URL } from './config.js';
 import { Book } from './models/book.model.js';
 
 const app = express();
@@ -14,15 +14,13 @@ app.get('/', (req, res) => {
   return res.status(234).send('<h1>Welcome to TuMoBooks App</h1>');
 });
 
-//* Route - Create New Book
+//* Route - POST New Book
 app.post('/books', async (req, res) => {
   try {
-    if(
-      !req.body.title ||
-      !req.body.author ||
-      !req.body.publishYear
-    ) {
-      return res.status(400).send({message: 'Fields: title, author, publishYear - required'})
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res
+        .status(400)
+        .send({ message: 'Fields: title, author, publishYear - required' });
     }
 
     const newBook = {
@@ -34,44 +32,63 @@ app.post('/books', async (req, res) => {
     const book = await Book.create(newBook);
 
     return res.status(201).send(book);
-  }
-  catch (err) {
-    console.error(err.message)
-    res.status(500).send({message: err.message})
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send({ message: err.message });
   }
 });
 
-//* Route - Read All Books
+//* Route - GET All Books
 app.get('/books', async (req, res) => {
   try {
     const books = await Book.find({});
 
     return res.status(200).json({
       count: books.length,
-      data: books
+      data: books,
     });
-  }
-  catch (err) {
+  } catch (err) {
     console.error(err.message);
     res.status(500).send({ message: err.message });
   }
 });
 
-//* Route - Read Book /:id
+//* Route - GET Book by /:id
 app.get('/books/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const book = await Book.findById(id);
 
     return res.status(200).json(book);
-  }
-  catch (err) {
+  } catch (err) {
     console.error(err.message);
-    res.status(500).send({message: err.message});
+    res.status(500).send({ message: err.message });
   }
-})
+});
 
-mongoose.connect(DB_URL)
+//* Route - PUT Book by /:id
+app.put('/books/:id', async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res
+        .status(400)
+        .send({ message: 'Fields: title, author, publishYear - required' });
+    }
+
+    const { id } = req.params;
+    const result = await Book.findByIdAndUpdate(id, req.body);
+
+    !result
+      ? res.status(404).json({ message: 'Fail! Book was not found' })
+      : res.status(200).send({ message: 'Success! Book was updated' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+mongoose
+  .connect(DB_URL)
   .then(() => {
     console.log('> Success: Application is connected to Database');
     app.listen(PORT, () => {
@@ -79,5 +96,5 @@ mongoose.connect(DB_URL)
     });
   })
   .catch((err) => {
-    console.error(err.message)
-  })
+    console.error(err.message);
+  });
