bed71c91c04072bf221d8a856636ea4b5225332a
solidados <geekbrains.pk@internet.ru>
2024-03-13 19:08:44 +0400

    feat: install cors pack and create middleware to handle CORS policy in backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
+
 import { PORT, DB_URL } from './config.js';
 import booksRouter from './routes/booksRouter.js';
 
@@ -8,9 +10,18 @@ const app = express();
 // middleware to parse req.body
 app.use(express.json());
 
+// middleware to handle CORS policy
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedMethods: ['Content-Type'],
+  })
+);
+
 app.get('/', (req, res) => {
   console.log(req);
-  return res.status(234).send('<h1>Welcome to TuMoBooks App</h1>');
+  return res.status(200).send('<h1>Welcome to TuMoBooks App</h1>');
 });
 
 app.use('/books', booksRouter);
