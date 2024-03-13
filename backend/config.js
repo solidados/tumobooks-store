7b98527bdaf6bf959e8b8c183932a5f5f866f47f
solidados <geekbrains.pk@internet.ru>
2024-03-13 15:34:04 +0400

    feat: add mongoose and dotenv packages; update index.js with routing and connect mongoose to database on MongoDB

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,7 @@
-export const PORT = 5555;
+const PORT = 5555;
+const DB_URL='mongodb+srv://guest:guest@tumobooksstore.gagrvt7.mongodb.net/tumobooks-store?retryWrites=true&w=majority&appName=TuMoBooksStore'
+
+export {
+  PORT,
+  DB_URL
+}
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 import express from 'express';
-import { PORT } from "./config.js";
+import mongoose from 'mongoose';
+import { PORT, DB_URL } from "./config.js";
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server is listening to PORT: ${PORT}`)
-})
+app.get('/', (req, res) => {
+  console.log(req)
+  return res.status(234).send('<h1>Welcome to TuMoBooks App</h1>')
+});
+
+mongoose.connect(DB_URL)
+  .then(() => {
+    console.log('> Success: Application is connected to Database');
+    app.listen(PORT, () => {
+      console.log(`> Success: Server is listening to PORT: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err.message)
+  })
