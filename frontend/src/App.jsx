aadc53be314e1299c146e416d74a8c2522c1379b
solidados <geekbrains.pk@internet.ru>
2024-03-13 20:35:14 +0400

    feat: add routes and elements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import CreateBook from './pages/CreateBook.jsx';
+import ShowBook from './pages/ShowBook.jsx';
+import EditBook from './pages/EditBook.jsx';
+import DeleteBook from './pages/DeleteBook.jsx';
+
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
+    </Routes>
   );
 };
 
